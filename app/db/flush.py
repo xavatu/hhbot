a60ac5b359ebbc86a0ff93d129454f7af934aa0d
xavatu <xavatu@gmail.com>
2025-06-19 12:22:26 +0300

    add flush

---
+++ app/db/flush.py
@@ -0,0 +1,33 @@
+import asyncio
+
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+
+import app.hh_integration.client.models  # noqa
+import app.hh_integration.negotiation.models  # noqa
+import app.hh_integration.resume.models  # noqa
+from app.db.base import Base
+
+TEST_DB_USER = "postgres"
+TEST_DB_PASSWORD = "postgres"
+TEST_DB_HOST = "localhost"
+TEST_DB_PORT = "5432"
+TEST_DB_NAME = "hh_bot"
+TEST_DB_URL = f"postgresql+asyncpg://{TEST_DB_USER}:{TEST_DB_PASSWORD}@{TEST_DB_HOST}:{TEST_DB_PORT}/{TEST_DB_NAME}"
+
+async_engine = create_async_engine(TEST_DB_URL, echo=True)
+async_session = async_sessionmaker(async_engine, expire_on_commit=False)
+
+
+async def create_all():
+    async with async_engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
+
+
+if __name__ == "__main__":
+    """
+    checking some new sqla 2.0 features
+    """
+    loop = asyncio.new_event_loop()
+    loop.run_until_complete(create_all())
+    loop.close()
