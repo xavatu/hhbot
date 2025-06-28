5cee44f6cbf2ec281e8b3f92bad6023d18a9b15f
xavatu <xavatu@gmail.com>
2025-06-28 19:47:02 +0300

    fix flush

--- db/flush.py
+++ db/flush.py
@@ -1,4 +1,5 @@
 import asyncio
+import os
 
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 
@@ -7,11 +8,11 @@ import models.negotiation  # noqa
 import models.resume  # noqa
 from models.base import Base
 
-TEST_DB_USER = "postgres"
-TEST_DB_PASSWORD = "postgres"
-TEST_DB_HOST = "localhost"
-TEST_DB_PORT = "5432"
-TEST_DB_NAME = "hh_bot"
+TEST_DB_USER = os.getenv("TEST_DB_USER", "postgres")
+TEST_DB_PASSWORD = os.getenv("TEST_DB_PASSWORD", "postgres")
+TEST_DB_HOST = os.getenv("TEST_DB_HOST", "localhost")
+TEST_DB_PORT = os.getenv("TEST_DB_PORT", "5432")
+TEST_DB_NAME = os.getenv("TEST_DB_NAME", "hh_bot")
 TEST_DB_URL = f"postgresql+asyncpg://{TEST_DB_USER}:{TEST_DB_PASSWORD}@{TEST_DB_HOST}:{TEST_DB_PORT}/{TEST_DB_NAME}"
 
 async_engine = create_async_engine(TEST_DB_URL, echo=True)
