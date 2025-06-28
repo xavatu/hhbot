186e3b0834e80516755c92bf67c348691b966b20
xavatu <xavatu@gmail.com>
2025-06-28 18:12:42 +0300

    add admin-panel container

--- app/admin_panel/config.py
+++ app/admin_panel/config.py
@@ -4,7 +4,7 @@ import uvicorn
 from fastapi import FastAPI
 
 host = os.getenv("API_HOST", "localhost")
-port = os.getenv("API_PORT", "8002")
+port = os.getenv("API_PORT", "8001")
 port = int(port)
 app_title = "Admin"
 
